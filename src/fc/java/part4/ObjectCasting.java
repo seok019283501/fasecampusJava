3d5e8b8625fbe0b83913073d2725122577a4df52
minseok <alsthr0338@gmail.com>
2024-03-04 10:49:02 +0900

    course1 part4 ch3-2

--- src/fc/java/part4/ObjectCasting.java
+++ src/fc/java/part4/ObjectCasting.java
@@ -5,10 +5,13 @@ import fc.java.model.*;
 
 public class ObjectCasting {
     public static void main(String[] args) {
-        Animal ani = new Dog();
+        Animal ani = new Dog(); // 업케스팅
         ani.eat();
         ani = new Cat();
         ani.eat();
 //        ani.night(); //다운케스팅을 하면 가능하다
+//        Cat c = (Cat)ani; //다운케스팅
+//        c.night();
+        ((Cat)ani).night();
     }
 }
