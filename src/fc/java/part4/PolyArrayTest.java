a71426b74fb85c769f2a855069d4191a47b78950
minseok <alsthr0338@gmail.com>
2024-03-04 11:50:26 +0900

    course1 part4 ch4-4

---
+++ src/fc/java/part4/PolyArrayTest.java
@@ -0,0 +1,24 @@
+package fc.java.part4;
+
+import fc.java.model.*;
+public class PolyArrayTest {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        Cat c = new Cat();
+        // Dog, Cat을 저장할 배열을 생성하시오.
+//        Animal[] ani = {new Dog(), new Cat()};
+        //다형성배열
+        Animal[] ani = new Animal[2];
+        ani[0] = d;
+        ani[1] = c;
+        display(ani);
+    }
+    public static void display(Animal[] ani){
+        for(int i = 0; i < ani.length; i++){
+            ani[i].eat();
+            if(ani[i] instanceof Cat){
+                ((Cat) ani[i]).night();
+            }
+        }
+    }
+}
